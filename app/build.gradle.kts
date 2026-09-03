77b30347bf1056b6d3c083b870d76ff9cbb1163a
pylikv-jpg <pylik.v@gmail.com>
2026-09-03 20:11:05 +0200

    Bump QueueWatch version for queue count fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pylikv.queuewatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1-queue-type-count"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
