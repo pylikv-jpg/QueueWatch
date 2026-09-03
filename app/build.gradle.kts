90e9c843503a724c3036b6d9c0b2715183057f6f
pylikv-jpg <pylik.v@gmail.com>
2026-09-03 20:54:29 +0200

    Bump QueueWatch for movement info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pylikv.queuewatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3-call-progress"
+        versionCode = 5
+        versionName = "0.1.4-queue-movement"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
