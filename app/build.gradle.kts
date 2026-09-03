b817c6e3bbae12a558d8197f522065b3abedc7bf
pylikv-jpg <pylik.v@gmail.com>
2026-09-03 20:25:59 +0200

    Bump QueueWatch version for swipe navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pylikv.queuewatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1-queue-type-count"
+        versionCode = 3
+        versionName = "0.1.2-swipe-navigation"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
