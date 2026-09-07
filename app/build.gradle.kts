3e0a57a50b3dfb6bfd316f21ae91d9dc3e51ee89
pylikv-jpg <pylik.v@gmail.com>
2026-09-07 16:43:29 +0200

    Bump QueueWatch next update to version 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pylikv.queuewatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.0"
+        versionCode = 8
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
