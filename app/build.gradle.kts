99171e339c918f29cca9cff7d083ae8fabae866e
pylikv-jpg <pylik.v@gmail.com>
2026-09-14 18:48:42 +0200

    Bump QueueWatch version to 1.0.3 (11) for Google Play update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pylikv.queuewatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.1"
+        versionCode = 11
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
